0048fa4bf4cfba1b0d1cacf5b071e46b75d6d253
Idris Adebowale <idrys01@gmail.com>
2023-09-22 11:29:05 +0000

    add python routes

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""Hello Flask Module
+
+Starts a flask web server on host 0.0.0.0 and port 5000
+"""
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_route():
+    """Handles the default route
+    """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb_route():
+    """Handles the /hbnb route
+    """
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_route(text):
+    """Handles the path /c/<text> with a variable string 'text'
+    """
+    return "C {}".format(text.replace("_", " "))
+
+
+@app.route('/python/<text>', strict_slashes=False)
+def python_route(text='is_cool'):
+    """Handles the path /python/<text> with a variable string 'text'
+    """
+    return "Python {}".format(text.replace("_", " "))
+
+
+@app.route('/python', strict_slashes=False)
+def python_is_cool():
+    """Handles the path /python with
+    """
+    return "Python is cool"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
