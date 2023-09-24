d90d0668e02c87d351bdad7756c2d4c65ea29a9d
Idris Adebowale <idrys01@gmail.com>
2023-09-24 09:40:47 -0700

    add route that list all states

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""This module define a route that list all states
+"""
+
+from flask import Flask, render_template
+import re
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def refresh_context(exception):
+    """Delete context for each request
+    """
+    storage.close()
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """Return List of all states
+    """
+    pattern = r"'name':\s'(?P<name>\w+)'.*?id':\s'(?P<id>[a-z-0-9]+)'"
+    all_states = storage.all(State)
+    data = [re.search(
+        pattern, state).groupdict() for state in all_states]
+    return render_template("7-states_list.html", states=data)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
