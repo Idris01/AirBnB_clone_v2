8e5518c09ed28597ebbfb385a072b1d33c88b5ee
Idris Adebowale <idrys01@gmail.com>
2023-09-27 01:11:30 -0700

    implement ordering of city in ascending order

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -17,11 +17,23 @@ def refresh_storage(exception):
 def cities_by_states():
     """Get All cities for a Given State
     """
+    all_data = []
     states = list(storage.all(State).values())
     states.sort(key=lambda x: x.name)
+    for state in states:
+        data = {}
+        data['name'] = state.name
+        cities = [dict(
+            id=city.id, name=city.name
+            ) for city in state.cities]
+        cities.sort(key=lambda x: x['name'])
+        data['cities'] = cities
+        data['id'] = state.id
+        all_data.append(data)
+
     return render_template(
             '8-cities_by_states.html',
-            states=states)
+            states=all_data)
 
 
 if __name__ == '__main__':
